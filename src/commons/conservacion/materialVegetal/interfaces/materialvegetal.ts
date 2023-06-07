c797536c27b7edabe27c52803dd71657bda07a83
Jerson Agudelo <133192260+JersonAgudelo@users.noreply.github.com>
2023-06-07 10:13:20 -0500

    cambios

--- src/commons/conservacion/materialVegetal/interfaces/materialvegetal.ts
+++ src/commons/conservacion/materialVegetal/interfaces/materialvegetal.ts
@@ -126,3 +126,113 @@ export interface IObjNursery {
 }
 
 
+export interface IObjRegistrarMortalidad {
+  id_vivero: number | null;
+	fecha_baja: string | Date | null;
+	motivo: string | null;
+  id_item_baja_viveros: number | null;
+  id_bien: number | null;
+  agno_lote: number | null;
+  nro_lote: number | null;
+  cod_etapa_lote: string | null;
+  consec_cuaren_por_lote_etapa: string | null;
+  cantidad_baja: number | null;
+  observaciones: string | null;
+  nro_posicion: number | null;
+}
+
+export interface IObjActualizarMortalidad {
+  id_item_baja_viveros:number | null;
+  id_bien: number | null;
+  agno_lote: number | null;
+  nro_lote: number | null;
+  cod_etapa_lote: string | null;
+  consec_cuaren_por_lote_etapa: string | null;
+  cantidad_baja: number | null;
+  observaciones: string | null;
+  nro_posicion: number | null;
+  justificacion_anulacion?: string | null;
+}
+
+export interface IObjObtenerViveroFiltro {
+  id_vivero: number | null;
+  nombre: string | null;
+  cod_municipio: string | null;
+  direccion: string | null;
+  area_mt2: number | null;
+  area_propagacion_mt2: number | null;
+  tiene_area_produccion: boolean | null;
+  tiene_areas_pep_sustrato: boolean | null;
+  tiene_area_embolsado: boolean | null;
+  cod_tipo_vivero: string | null;
+  fecha_inicio_viverista_actual: string | Date | null;
+  cod_origen_recursos_vivero: string | null;
+  fecha_creacion: string | Date | null;
+  en_funcionamiento: boolean | null;
+  fecha_ultima_apertura: string | Date | null;
+  justificacion_apertura: string | null;
+  fecha_cierre_actual: string | Date | null;
+  justificacion_cierre: string | null;
+  vivero_en_cuarentena: string | null;
+  fecha_inicio_cuarentena: string | Date | null;
+  justificacion_cuarentena: string | null;
+  ruta_archivo_creacion: string | null;
+  activo: boolean | null;
+  item_ya_usado: boolean | null;
+  id_viverista_actual: number | null;
+  id_persona_crea: number | null;
+  id_persona_abre: number | null;
+  id_persona_cierra: number | null;
+  id_persona_cuarentena: number | null;
+}
+
+export interface IObjObtenerMaterialVegetal {
+  id_inventario_vivero: number | null;
+  id_bien: number | null;
+  codigo_bien: string | null;
+  nombre_bien: string | null;
+  agno_lote: number | null;
+  nro_lote: number | null;
+  cod_etapa_lote: string | null;
+  desc_etapa_lote: string | null;
+  saldo_disponible: number | null;
+  unidad_medida: string | null;
+  registros_cuarentena: string | null;
+}
+
+export interface IObjObtenerMortalidad {
+  id_baja: number | null;
+  tipo_baja: string | null;
+  nro_baja_por_tipo: number | null;
+  fecha_baja: string | Date | null;
+  fecha_registro: string | Date | null;
+  motivo: string | null;
+  baja_anulado:boolean | null;
+  justificacion_anulacion: string | null;
+  fecha_anulacion: string | Date | null;
+  ruta_archivo_soporte: string | null;
+  id_vivero: number | null;
+  nombre_vivero: string | null;
+  id_persona_baja: number | null;
+  persona_baja: string | null;
+  id_persona_anula: number | null;
+  persona_anula: string | null;
+  items_mortalidad: string | null;
+}
+
+export interface IObjDatosMortalidad {
+  id_item_baja_viveros: number | null;
+  codigo_bien: string | null;
+  nombre_bien: string | null;
+  unidad_medida: string | null;
+  desc_etapa_lote: string | null;
+  agno_lote: number | null;
+  nro_lote: number | null;
+  cod_etapa_lote: string | null;
+  consec_cuaren_por_lote_etapa: string | null;
+  cantidad_baja: number | null;
+  observaciones: string | null;
+  nro_posicion: number | null;
+  id_baja: number | null;
+  id_bien: number | null;
+}
